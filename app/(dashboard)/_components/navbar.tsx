fb36b027201402e1f9abc9cf0ccb3e9494a00444
Sandeep <sandeeppakho55@gmail.com>
2024-03-03 11:15:13 +0530

    ADDED: Dashboard layout and sidebar

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+
+const Navbar = () => {
+  return (
+    <div className=" flex items-center gap-x-4 p-5">
+      <div className=" hidden lg:flex lg:flex-1">{/* TODO: Add Search */}</div>
+      <UserButton />
+    </div>
+  );
+};
+
+export default Navbar;
